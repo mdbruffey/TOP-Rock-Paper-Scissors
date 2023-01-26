33fe61e1b138bbabff1fe06cd4ac092d716b0573
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-01-26 12:21:12 -0600

    Fix errors in script.js
    Fixed issue with formatting of template literals
    Fixed issue with computerChoice not actually generating "scissors"
    Fixed variable typo

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-choices = ["rock", "paper", "scissors"];
+let choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
-    return Math.floor(Math.random() * 2); //returns 0,1,2 to indicate choice
+    return Math.floor(Math.random() * 3); //returns 0,1,2 to indicate choice
 }
 
 function playRound(playerSelection, computerSelection){
     diff = playerSelection - computerSelection;
-    if (dif === 1 || dif === -2){
-        return `You win! $(choices[playerSelection]) beats $(choices[computerSelection])`;
+    if (diff === 1 || diff === -2){
+        return `You win! ${choices[playerSelection]} beats ${choices[computerSelection]}`;
     }
-    else if (dif === 0){
-        return `You tied. You both picked $(choices[playerSelection])`;
+    else if (diff === 0){
+        return `You tied. You both picked ${choices[playerSelection]}`;
     }
     else {
-        return `You lost! $(choices[computerSelection]) beats $(choices[playerSelection])`
+        return `You lost! ${choices[computerSelection]} beats ${choices[playerSelection]}`
     }
 }
 
@@ -22,23 +22,29 @@ function game(){
     let ccount = 0
     let playerSelection;
     for (let i=0; i < 5; i++){
-        playerSelection = choices.findIndex(prompt("Enter a move (Rock, Paper, Scissors: )").toLowerCase());
+        let string = prompt("Enter a move (Rock, Paper, Scissors): ").toLowerCase();
+        playerSelection = choices.indexOf(string);
         result = playRound(playerSelection,getComputerChoice());
         if (result.includes("win")){
             pcount++;
         }
-        else if (result.includes("lose")){
+        else if (result.includes("lost")){
             ccount++;
         }
-        console.log(`$(result) : $(pcount)-$(ccount)`)
+        console.log(`${result} : ${pcount}-${ccount}`)
     }
     if (pcount > ccount){
-        console.log(`You won the game $(pcount)-$(ccount)`)
+        console.log(`You won the game ${pcount}-${ccount}`)
     }
     else if(ccount > pcount){
-        console.log(`You lost the game $(pcount)-$(ccount)`)
+        console.log(`You lost the game ${pcount}-${ccount}`)
     }
     else{
-        console.log(`The game was a tie $(pcount)-$(ccount)`)
+        console.log(`The game was a tie ${pcount}-${ccount}`)
     }
 }
+
+for (let i=0; i < 10; i++){
+    console.log(getComputerChoice())
+}
+game()
