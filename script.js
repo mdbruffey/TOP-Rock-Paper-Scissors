fb6e2bdc68f6b5ffbac3967a72be61693c4bb742
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-01-26 11:54:48 -0600

    Add game loop
    Game consists of five rounds. At the end of five rounds whichever player
    has the highest score wins.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-choices = ["Rock", "Paper", "Scissors"];
+choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     return Math.floor(Math.random() * 2); //returns 0,1,2 to indicate choice
@@ -14,5 +14,31 @@ function playRound(playerSelection, computerSelection){
     }
     else {
         return `You lost! $(choices[computerSelection]) beats $(choices[playerSelection])`
+    }
+}
+
+function game(){
+    let pcount = 0;
+    let ccount = 0
+    let playerSelection;
+    for (let i=0; i < 5; i++){
+        playerSelection = choices.findIndex(prompt("Enter a move (Rock, Paper, Scissors: )").toLowerCase());
+        result = playRound(playerSelection,getComputerChoice());
+        if (result.includes("win")){
+            pcount++;
+        }
+        else if (result.includes("lose")){
+            ccount++;
+        }
+        console.log(`$(result) : $(pcount)-$(ccount)`)
+    }
+    if (pcount > ccount){
+        console.log(`You won the game $(pcount)-$(ccount)`)
+    }
+    else if(ccount > pcount){
+        console.log(`You lost the game $(pcount)-$(ccount)`)
+    }
+    else{
+        console.log(`The game was a tie $(pcount)-$(ccount)`)
     }
 }
