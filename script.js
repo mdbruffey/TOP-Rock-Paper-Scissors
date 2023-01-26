f63d1f4c244e6c8f282f09afe6b212a9850c070c
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-01-26 11:44:10 -0600

    Add playRound function
    Takes player and computer selections and returns a string indicating
    the winner of the round.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+choices = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice(){
-    return Math.floor(Math.random() * 2) //returns 0,1,2 to indicate choice
+    return Math.floor(Math.random() * 2); //returns 0,1,2 to indicate choice
+}
+
+function playRound(playerSelection, computerSelection){
+    diff = playerSelection - computerSelection;
+    if (dif === 1 || dif === -2){
+        return `You win! $(choices[playerSelection]) beats $(choices[computerSelection])`;
+    }
+    else if (dif === 0){
+        return `You tied. You both picked $(choices[playerSelection])`;
+    }
+    else {
+        return `You lost! $(choices[computerSelection]) beats $(choices[playerSelection])`
+    }
 }
